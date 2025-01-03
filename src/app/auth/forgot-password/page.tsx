0b355a59f8534484160238c164c0cf9878460bec
omalmaleesha <omalmaleesha03@gmail.com>
2025-01-03 15:06:06 +0530

    change color

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -6,10 +6,10 @@ export default function Page() {
 
     return (
         <div
-            className="flex min-h-screen items-center justify-center bg-gradient-to-b from-purple-100 via-purple-200 to-purple-300">
+            className="flex min-h-screen items-center justify-center">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg ring-1 ring-purple-200">
-                <h1 className="text-3xl font-extrabold text-purple-900 text-center">Forgot Password</h1>
-                <p className="text-sm text-purple-700 text-center">
+                <h1 className="text-3xl font-extrabold text-black text-center">Forgot Password</h1>
+                <p className="text-sm text-black text-center">
                     Enter your email address, and we’ll send you a link to reset your password. Or choose another
                     recovery option.
                 </p>
@@ -24,7 +24,7 @@ export default function Page() {
                     </div>
                         <Button
                             type="submit"
-                            className="w-full py-2 px-4 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                            className="w-full py-2 px-4 bg-black text-white rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500"
                         >reset password
                         </Button>
                 </form>
