9f617f9489b8d99a75699c3660c459675cca883a
SandakelumElvitigala <kalpaselvitigala@gmail.com>
2025-01-11 08:36:05 +0530

    Optimize imports

--- src/app/pages/landing-page/landing-page.tsx
+++ src/app/pages/landing-page/landing-page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { Navbar } from "../../../components/navbar"
-import { Hero } from "../../../components/hero"
-import { Features } from "../../../components/features"
-import { Templates } from "../../../components/template"
-import { Footer } from "../../../components/footer"
+import { Navbar } from "@/components/navbar"
+import { Hero } from "@/components/hero"
+import { Features } from "@/components/features"
+import { Templates } from "@/components/template"
+import { Footer } from "@/components/footer"
 import VantaBackground from "../../../components/amimation-landingpage"
 
 
